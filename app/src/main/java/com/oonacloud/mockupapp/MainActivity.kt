011113bd9c4062577ecc25b031cf4e48366b89dd
David LAVERDA <david.laverda@aavamobile.com>
2020-03-25 16:22:59 +0100

    fix: clean code

--- app/src/main/java/com/oonacloud/mockupapp/MainActivity.kt
+++ app/src/main/java/com/oonacloud/mockupapp/MainActivity.kt
@@ -12,52 +12,35 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.alert
 import kotlin.system.exitProcess
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Package name
         val packageName = "com.oonacloud.checkoutapp"
         val urlWebPageName = "http://app.domaine.com/WebApp.html"
 
-        // Click listener for button widget
         button.setOnClickListener{
-            // Launch the app programmatically
             launchApp(packageName)
         }
 
         buttonClose.setOnClickListener {
-            // Close
-            finish();
-            exitProcess(0);
+            finish()
+            exitProcess(0)
         }
 
         buttonOpenWebApp.setOnClickListener {
-            // Lunch the web app
             openUrlInChrome(urlWebPageName)
         }
-
     }
 
-
-    // Custom method to launch an app
     private fun launchApp(packageName: String) {
-        // Get an instance of PackageManager
         val pm = applicationContext.packageManager
-
-        // Initialize a new Intent
         val intent:Intent? = pm.getLaunchIntentForPackage(packageName)
-
-        // Add category to intent
         intent?.addCategory(Intent.CATEGORY_LAUNCHER)
-
         try {
             applicationContext.startActivity(intent)
         } catch (e: Exception) {
-            // Application was just removed?
             alert("$packageName => startActivity(intent) : $e") {
                 title = "Alert"
                 yesButton { }
@@ -77,19 +60,15 @@ class MainActivity : AppCompatActivity() {
                 startActivity(i)
             } catch (e: ActivityNotFoundException) {
                 val uri = Uri.parse(url)
-                // Chrome is probably not installed
-                // OR not selected as default browser OR if no Browser is selected as default browser
                 val i = Intent(Intent.ACTION_VIEW, uri)
                 startActivity(i)
             }
-        } catch (ex: Exception) {
-            //
+        } catch (e: Exception) {
+            alert("openUrlInChrome error : $e") {
+                title = "Alert"
+                yesButton { }
+                noButton { }
+            }.show()
         }
     }
-
-}
-
-// Extension function to show toast message
-fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
